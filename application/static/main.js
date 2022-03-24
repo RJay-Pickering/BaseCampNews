b17d62b5723bff307a27bd30254b11201c5afdc3
RJay-Pickering <rjaypickering@gmail.com>
2022-03-24 16:12:46 -0500

    Quinton did css in navbar and fixed styling of newsbox to conseal the links, Will Messed around with the api stuff and did some javascript functionallity, RJay tried to fix the search box to make it search on google or bing and helped quinton with styling.

--- application/static/main.js
+++ application/static/main.js
@@ -3,20 +3,28 @@
 var news = document.getElementById("news");
 
 fetch(
-  "https://newsdata.io/api/1/news?apikey=pub_56870dc5f12b0d125f359b92537755980553&q=apple"
+  "https://newsdata.io/api/1/news?apikey=pub_56870dc5f12b0d125f359b92537755980553&q=stocks"
 )
   .then((response) => {
     return response.json();
   })
   .then((data) => {
+    console.log(data.results);
     Array.from(data.results).forEach(function (a) {
       console.log(a);
       const aLink = document.createElement("a");
       aLink.href = `${a.link}`;
       aLink.target = "_blank";
       const article = document.createElement("div");
+      const desc = document.createElement("p");
+      desc.innerText = a.description;
+      desc.style.fontSize = "14px";
+      const date = document.createElement("p");
+      date.innerText = `Published: ${a.pubDate}`;
       article.classList.add("card");
       article.innerHTML = a.title;
+      article.appendChild(desc);
+      article.appendChild(date);
       aLink.classList.add("aLink");
       aLink.appendChild(article);
       news.appendChild(aLink);

72ee013599c1a8c956da9eefb8b3193fda8e4463
RJay-Pickering <rjaypickering@gmail.com>
2022-05-05 15:32:04 -0500

    test

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-90&=zl6%xuh7o!%qro(yx(k_pjm+80cqi^v@v6#kyjymvx1gxk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 

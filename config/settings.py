e8020f114aa9f5e781a23dd86cfc545e03915feb
RJay-Pickering <rjaypickering@gmail.com>
2022-05-05 14:36:56 -0500

    Automatic configuration with django-heroku

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import django_heroku
+django_heroku.settings(locals())
+
 
 from pathlib import Path
 

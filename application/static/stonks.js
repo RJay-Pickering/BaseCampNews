1b40ffd34a73a40b2849444644420db51022f32d
RJay-Pickering <rjaypickering@gmail.com>
2022-05-05 14:01:53 -0500

    RJay - Finnished the tv media screens and started the web development

--- application/static/stonks.js
+++ application/static/stonks.js
@@ -21,61 +21,61 @@ function showPosition(position) {
     });
 }
 
-// const query = document.getElementById("head").innerText;
+const query = document.getElementById("head").innerText;
 
-// fetch(
-//   `https://newsdata.io/api/1/news?apikey=pub_56870dc5f12b0d125f359b92537755980553&q=${query}&country=us,gb,jp&language=en,jp`
-// )
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     apples = 0;
-//     Array.from(data.results).forEach(function (a) {
-//       var randomAdPlace = Math.floor(Math.random() * 10) + 1;
-//       console.log(randomAdPlace);
-//       if (randomAdPlace === 5) {
-//         randomAdFunction(apples);
-//         apples++;
-//       } else {
-//         apples++;
-//         const aLink = document.createElement("a");
-//         aLink.href = `${a.link}`;
-//         aLink.target = "_blank";
-//         const article = document.createElement("div");
-//         const desc = document.createElement("p");
-//         desc.innerText = a.description;
-//         desc.style.fontSize = "10px";
-//         const date = document.createElement("p");
-//         date.innerText = `Published: ${a.pubDate}`;
-//         article.classList.add("card");
-//         const image = document.createElement("img");
-//         if (a.image_url == null) {
-//           image.src =
-//             "https://miro.medium.com/max/1400/1*T9VUDALam3DIS0wHDWrxBg.png";
-//           image.style.width = "250px";
-//           image.style.height = "200px";
-//         } else {
-//           image.src = a.image_url;
-//           image.style.width = "250px";
-//           image.style.height = "200px";
-//         }
-//         image.classList.add("apiImage");
-//         const title = document.createElement("p");
-//         title.innerText = a.title;
-//         article.appendChild(title);
-//         article.appendChild(image);
-//         article.appendChild(desc);
-//         article.appendChild(date);
-//         aLink.classList.add("aLink");
-//         aLink.appendChild(article);
-//         const one = document.getElementById("one");
-//         one.classList.add("column");
-//         one.appendChild(aLink);
-//         console.log(article);
-//       }
-//     });
-//   });
+fetch(
+  `https://newsdata.io/api/1/news?apikey=pub_56870dc5f12b0d125f359b92537755980553&q=${query}&country=us,gb,jp&language=en,jp`
+)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    apples = 0;
+    Array.from(data.results).forEach(function (a) {
+      var randomAdPlace = Math.floor(Math.random() * 10) + 1;
+      console.log(randomAdPlace);
+      if (randomAdPlace === 5) {
+        randomAdFunction(apples);
+        apples++;
+      } else {
+        apples++;
+        const aLink = document.createElement("a");
+        aLink.href = `${a.link}`;
+        aLink.target = "_blank";
+        const article = document.createElement("div");
+        const desc = document.createElement("p");
+        desc.innerText = a.description;
+        desc.style.fontSize = "10px";
+        const date = document.createElement("p");
+        date.innerText = `Published: ${a.pubDate}`;
+        article.classList.add("card");
+        const image = document.createElement("img");
+        if (a.image_url == null) {
+          image.src =
+            "https://miro.medium.com/max/1400/1*T9VUDALam3DIS0wHDWrxBg.png";
+          image.style.width = "250px";
+          image.style.height = "200px";
+        } else {
+          image.src = a.image_url;
+          image.style.width = "250px";
+          image.style.height = "200px";
+        }
+        image.classList.add("apiImage");
+        const title = document.createElement("p");
+        title.innerText = a.title;
+        article.appendChild(title);
+        article.appendChild(image);
+        article.appendChild(desc);
+        article.appendChild(date);
+        aLink.classList.add("aLink");
+        aLink.appendChild(article);
+        const one = document.getElementById("one");
+        one.classList.add("column");
+        one.appendChild(aLink);
+        console.log(article);
+      }
+    });
+  });
 
 function randomAdFunction(i) {
   fetch("/static/ads.json")
@@ -114,7 +114,7 @@ function randomAdFunction(i) {
 
 const stonks = document.getElementById("stocks");
 fetch(
-  "http://api.marketstack.com/v1/eod?access_key=d069da6bc4f3018084108107fca36373&symbols=AAPL,AMZN,JPM,TSLA,DOW&limit=5"
+  "http://api.marketstack.com/v1/eod?access_key=bd6e6a6ed1c65cc7677fa777cdab6f8b&symbols=AAPL,AMZN,JPM,TSLA,DOW&limit=5"
 )
   .then((response) => {
     return response.json();
@@ -126,13 +126,30 @@ fetch(
       const price = document.createElement("p");
       price.innerHTML = s.adj_close;
       const symbol = document.createElement("p");
+      const logo = document.createElement("img");
+      logo.id = 'logo';
+      symbol.id = 'symbol';
       symbol.innerText = s.symbol;
+      console.log(symbol.innerText + 'sIT');
+      console.log(s.symbol);
+      logo.style.width = "50px";
       symbol.classList.add("forecastDays");
       stock.classList.add("forecastCard");
       stock.appendChild(symbol);
+      stock.appendChild(logo);
       stock.appendChild(price);
+      console.log(stock);
       stocks.appendChild(stock);
     });
+    stockHead = document.getElementsByClassName('stocks');
+    Array.from(stockHead).forEach(function(s) {
+      console.log(s)
+      const name = document.getElementById('symbol')
+      if (name.value() = 'AAPL') {
+        var logo = document.getElementById('logo').src = '/static/Apple_logo_black.svg'
+      }
+    })
+
   });
 
 function openNav() {

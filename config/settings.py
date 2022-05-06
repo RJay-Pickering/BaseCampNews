a344d72194eaf4f4226928d9eea7ea461d7d3f2c
RJay-Pickering <rjaypickering@gmail.com>
2022-05-06 08:40:43 -0500

    took coorsheaders out

--- config/settings.py
+++ config/settings.py
@@ -43,11 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
-    '**corsheaders.middleware.CorsMiddleware**',
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',

4eaf2ea703ebf3d3364cd62b45e922c09e0adc4e
RJay-Pickering <rjaypickering@gmail.com>
2022-10-25 11:53:15 -0500

    fixing sign up page

--- application/forms.py
+++ application/forms.py
@@ -3,6 +3,9 @@ from .models import Customer
 from django.forms import ModelForm
 
 class CreateUserForm(UserCreationForm):
+
+    
+
     class Meta:
         model = Customer
         fields = ['username', 'email', 'password1', 'password2', 'interest1', 'interest2', 'interest3']

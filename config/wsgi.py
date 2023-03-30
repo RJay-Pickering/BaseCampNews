55489dc0459575780f00fc4d7790279854f437dd
RJay-Pickering <rjaypickering@gmail.com>
2023-03-30 10:42:12 -0500

    fix what i took out

--- config/wsgi.py
+++ config/wsgi.py
@@ -18,5 +18,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 application = get_wsgi_application()
 
 
-# application = WhiteNoise(application, root="/path/to/static/files")
-# application.add_files("/path/to/more/static/files", prefix="more-files/")
+application = WhiteNoise(application, root="/path/to/static/files")
+application.add_files("/path/to/more/static/files", prefix="more-files/")
